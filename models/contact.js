bf47caf6a02d0199f34a4c90ea52a443a088f61f
axtellion <slark19735@gmail.com>
2022-11-07 17:48:22 +0200

    end hw

--- models/contact.js
+++ models/contact.js
@@ -1,7 +1,7 @@
 const { Schema, model } = require("mongoose");
-const { handleSaveErrors } = require("../helpers");
-
 const Joi = require("joi");
+
+const { handleSaveErrors } = require("../helpers");
 
 const contactSchema = new Schema(
   {
@@ -18,6 +18,11 @@ const contactSchema = new Schema(
     favorite: {
       type: Boolean,
       default: false,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+      required: true,
     },
   },
   { versionKey: false, timeseries: true }
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -4,27 +4,34 @@ const ctrl = require("../../controllers/contacts");
 
 const router = express.Router();
 
-const { validateBody } = require("../../middlewares");
+const { validateBody, authenticate } = require("../../middlewares");
 const { ctrlWrapper } = require("../../helpers");
 
 const { schemas } = require("../../models/contact");
 
-router.get("/", ctrlWrapper(ctrl.getAll));
+router.get("/", authenticate, ctrlWrapper(ctrl.getAll));
 
-router.get("/:contactId", ctrlWrapper(ctrl.getById));
+router.get("/:contactId", authenticate, ctrlWrapper(ctrl.getById));
 
-router.post("/", validateBody(schemas.addSchema), ctrlWrapper(ctrl.add));
+router.post(
+  "/",
+  authenticate,
+  validateBody(schemas.addSchema),
+  ctrlWrapper(ctrl.add)
+);
 
-router.delete("/:contactId", ctrlWrapper(ctrl.removeById));
+router.delete("/:contactId", authenticate, ctrlWrapper(ctrl.removeById));
 
 router.put(
   "/:contactId",
+  authenticate,
   validateBody(schemas.addSchema),
   ctrlWrapper(ctrl.updateById)
 );
 
 router.patch(
   "/:contactId/favorite",
+  authenticate,
   validateBody(schemas.updateFavoriteSchema),
   ctrlWrapper(ctrl.updateFavorite)
 );
