676e78aefdc9ff357ae423031be88f59431ce993
axtellion <slark19735@gmail.com>
2022-11-09 17:03:00 +0200

    end_hw_5

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 
-const { validateBody, authenticate } = require("../../middlewares");
+const { validateBody, authenticate, upload } = require("../../middlewares");
 const { ctrlWrapper } = require("../../helpers");
 
 const { schemas } = require("../../models/user");
@@ -24,5 +24,12 @@ router.post(
 router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent));
 
 router.get("/logout", authenticate, ctrlWrapper(ctrl.logout));
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  ctrlWrapper(ctrl.updateAvatar)
+);
 
 module.exports = router;
