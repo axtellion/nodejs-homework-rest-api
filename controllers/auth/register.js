343fd9234e94a3ec05837e20a4b2a7a078e68a9d
axtellion <slark19735@gmail.com>
2022-11-13 02:08:49 +0200

    end hw_6

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,9 +1,13 @@
 const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
+require("dotenv").config();
 
 const { User } = require("../../models/user");
 
-const { RequestError } = require("../../helpers");
+const { BASE_URL } = process.env;
+
+const { RequestError, sendEmail } = require("../../helpers");
 
 const register = async (req, res) => {
   const { email, password } = req.body;
@@ -13,11 +17,22 @@ const register = async (req, res) => {
   }
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
   const result = await User.create({
     email,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
+
+  const mail = {
+    to: email,
+    subject: "Verify email",
+    html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationToken}">Click to verify you email</a>`,
+  };
+
+  await sendEmail(mail);
+
   res.status(201).json({
     email: result.email,
   });
